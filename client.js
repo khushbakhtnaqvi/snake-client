a14ccde52833181aba08c4d51f0ffd544bc77d3f
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-11-03 14:21:54 -0400

    snake moving and game can now be played

--- client.js
+++ client.js
@@ -1,9 +1,9 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function (connection) {
   const conn = net.createConnection({
-    host: "172.28.222.243", // IP address here,
+    host: "165.227.47.243", // IP address here,
     port: 50541 // PORT number here,
   });
 
@@ -13,10 +13,6 @@ const connect = function () {
     // code that does something when the connection is first established
     console.log("Successfully connected to game server");
     conn.write("Name: KBZ");
-    setInterval(() => conn.write("Move: up"), 1000);
-    setInterval(() => conn.write("Move: left"), 2000);
-    setInterval(() => conn.write("Move: down"), 3000);
-    setInterval(() => conn.write("Move: right"), 4000);
   });
 
   conn.on("end", () => {
--- input.js
+++ input.js
@@ -1,5 +1,9 @@
 const stdin = process.stdin;
-const setupInput = function () {
+let connection;
+let timer;
+let time = 150;
+const setupInput = function (conn) {
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -10,10 +14,31 @@ const setupInput = function () {
 const handleUserInput = function () {
   // your code here
   stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
+    //if (key === '\u0003') {
+    //  process.exit();
+    //}
+    switch (key) {
+      case '\u0003':
+        process.exit();
+        break;
+      case '\u0077':
+        clearInterval(timer);
+        timer = setInterval(() => connection.write("Move: up"), time);
+        break;
+      case '\u0061':
+        clearInterval(timer);
+        timer = setInterval(() => connection.write("Move: left"), time);
+        break;
+      case '\u0073':
+        clearInterval(timer);
+        timer = setInterval(() => connection.write("Move: down"), time);        
+        break;
+      case '\u0064':
+        clearInterval(timer);
+        timer = setInterval(() => connection.write("Move: right"), time);
+        break;
+      }
   });
 };
 
-module.exports = {setupInput};
+module.exports = {setupInput, stdin};
