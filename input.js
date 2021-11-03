be7600a28a12267fc78c3af5ed0bb9618c0ad61a
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-11-03 13:21:48 -0400

    made input module and called it from play.js but exporting it

---
+++ input.js
@@ -0,0 +1,19 @@
+const stdin = process.stdin;
+const setupInput = function () {
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
+module.exports = {setupInput};
